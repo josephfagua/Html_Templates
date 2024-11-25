4dd8770bf8b6c31e0ff05178ca2e87535d9bab98
josephfagua <faguaja@g.cofc.edu>
2024-11-25 14:48:21 -0500

    Update NavbarStyle-1.js
    Fixed the company notice to actually be a comment, due to it previously preventing javascript from working

--- static/javascript/NavbarStyle-1.js
+++ static/javascript/NavbarStyle-1.js
@@ -1,10 +1,9 @@
-<!--
-    This javascript file was developed by the Crattivo Department of Gocreattivo.
-    It is designed to perform the necessary functions required by the NavbarStyle-1.html template.
-    Our team specializes in Full Stack Development, Database Management, API Integration,
-    Frontend & Backend Coordination, and Code Optimization.
-    Gocreattivo is committed to delivering user-friendly, high-performance web solutions.
--->
+//     This javascript file was developed by the Crattivo Department of Gocreattivo.
+//     It is designed to perform the necessary functions required by the NavbarStyle-1.html template.
+//     Our team specializes in Full Stack Development, Database Management, API Integration,
+//     Frontend & Backend Coordination, and Code Optimization.
+//     Gocreattivo is committed to delivering user-friendly, high-performance web solutions.
+
 
 function toggleNav() {
     document.body.classList.toggle("mobile-nav-open");
